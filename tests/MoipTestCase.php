27c7264a764405cdcc7de0c87b44784390e8924b
Jean Cesar Garcia <jeancesargarcia@gmail.com>
2015-10-08 22:37:30 -0300

    base moip class to tests

---
+++ tests/MoipTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Moip\Tests;
+
+use Mockery as m;
+use Moip\Moip;
+use Moip\MoipAuthentication;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+* class MoipTestCase
+*/
+abstract class MoipTestCase extends TestCase
+{
+	/**
+	 * Intance of \Moip\Moip.
+	 *
+	 * @var \Moip\Moip
+	 **/
+	protected $moip;
+
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+	public function setUp()
+	{
+		$this->moip = new Moip(m::mock(MoipAuthentication::class));
+	}
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+	public function tearDown()
+	{
+		m::close();
+	}
+}
