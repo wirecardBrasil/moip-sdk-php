87bcc9a30d33dd6d87f03e3cab1303d528e5032d
Jean Cesar Garcia <jeancesargarcia@gmail.com>
2015-10-08 22:44:21 -0300

    Test if endpoint production is valid

--- tests/MoipTest.php
+++ tests/MoipTest.php
@@ -9,5 +9,14 @@ use Moip\Tests\MoipTestCase;
 */
 class MoipTest extends MoipTestCase
 {
-	
+	/**
+	 * Test if endpoint production is valid.
+	 */
+	public function testShouldReceiveEndpointProductionIsValid()
+	{
+		$endpoint_production = 'api.moip.com.br';
+		$const_endpoint_production = constant('\Moip\Moip::ENDPOINT_PRODUCTION');
+
+		$this->assertEquals($endpoint_production, $const_endpoint_production);
+	}
 }
