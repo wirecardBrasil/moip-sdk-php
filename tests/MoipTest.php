62d1ee8b6cbdc45de282a35e1279fe4bb7e9bb10
Jean Cesar Garcia <jeancesargarcia@gmail.com>
2015-10-08 22:47:52 -0300

    Test if endpoint sandbox is valid

--- tests/MoipTest.php
+++ tests/MoipTest.php
@@ -19,4 +19,15 @@ class MoipTest extends MoipTestCase
 
 		$this->assertEquals($endpoint_production, $const_endpoint_production);
 	}
+
+	/**
+	 * Test if endpoint sandbox is valid.
+	 */
+	public function testShouldReceiveSandboxProductionIsValid()
+	{
+		$endpoint_sandbox = 'sandbox.moip.com.br';
+		$const_endpoint_sandbox = constant('\Moip\Moip::ENDPOINT_SANDBOX');
+
+		$this->assertEquals($endpoint_sandbox, $const_endpoint_sandbox);
+	}
 }
