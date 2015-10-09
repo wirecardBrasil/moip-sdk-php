e0bb88b1b65088f6b4b78d2d8b82c75eea43c968
Jean C. Garcia <jeancesargarcia@gmail.com>
2015-10-09 18:56:33 -0300

    test if const \Moip\Resource\Customer::PATH is valid

--- tests/Resource/CustomerTest.php
+++ tests/Resource/CustomerTest.php
@@ -9,4 +9,14 @@ use Moip\Tests\MoipTestCase;
 */
 class CustomerTest extends MoipTestCase
 {
+	/**
+	 * Test if const \Moip\Resource\Customer::PATH is valid.
+	 */
+	public function testShouldReceiveConstPathIsValid()
+	{
+		$path = 'customers';
+		$const_path = constant('\Moip\Resource\Customer::PATH');
+
+		$this->assertEquals($path, $const_path);
+	}
 }
