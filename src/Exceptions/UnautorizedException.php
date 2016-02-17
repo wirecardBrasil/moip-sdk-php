0728a589238268e43f6ae2b9e8d9e45e78d62ba4
Gabriel Menezes <gabrielmenezesvi@gmail.com>
2016-02-17 15:27:23 -0200

    Create Moip\Exceptions and class for the 401 exception

---
+++ src/Exceptions/UnautorizedException.php
@@ -0,0 +1,22 @@
+<?php
+namespace Moip\Exceptions;
+
+
+/**
+ * Class AuthException
+ *
+ * This exception is thrown when the Moip API returns a 401 http code.
+ * The error message was extracted from the documentation.
+ *
+ * @package Moip\Exceptions
+ */
+class UnautorizedException extends \RuntimeException
+{
+
+    public function __construct()
+    {
+        // error string is in portuguese because the error descriptions returned by the API are also in portuguese
+        parent::__construct("[401] Erro de Autenticação. confirme se sua autenticação pode realizar a ação desejada e se está usando o ambiente correto, lembre-se que as chaves de produção e de sandbox são distintas.",
+            401);
+    }
+}
