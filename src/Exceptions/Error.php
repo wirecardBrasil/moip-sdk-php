88a6eda6e251cf9c5cf04883dfec8186bdae6545
Gabriel Menezes <gabrielmenezesvi@gmail.com>
2016-02-17 16:34:11 -0200

    Create validation exception and a class for representing errors

---
+++ src/Exceptions/Error.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Moip\Exceptions;
+
+
+/**
+ * Class Error.
+ *
+ * Represents an error returned by the API.
+ *
+ * @package Moip\Exceptions
+ */
+class Error
+{
+
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Error constructor.
+     *
+     * Represents an error return by the API. Commonly used by {@see \Moip\Exceptions\ValidationException}
+     *
+     * @param string $code        unique error identifier.
+     * @param string $path        represents the field where the error ocurred.
+     * @param string $description error description.
+     */
+    public function __construct($code, $path, $description)
+    {
+        $this->code = $code;
+        $this->path = $path;
+        $this->description = $description;
+    }
+
+    /**
+     *
+     * Returns the unique alphanumeric identifier of the error, ie.: "API-1"
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     *
+     * Returns the dotted string representing the field where the error ocurred, ie.: "customer.birthDate"
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     *
+     * Returns the error description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+}
---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Moip\Exceptions;
+
+
+class ValidationException extends \RuntimeException
+{
+    /**
+     * @var int
+     */
+    private $statusCode;
+
+    /**
+     * @var string
+     */
+    private $statusMessage;
+
+    /**
+     * @var Error[]
+     */
+    private $errors;
+
+    /**
+     * ValidationException constructor.
+     *
+     * Exception thrown when the moip API returns a 4xx http code.
+     * Indicates that an invalid value was passed.
+     *
+     * @param string  $statusCode
+     * @param int     $statusMessage
+     * @param Error[] $errors
+     *
+     */
+    public function __construct($statusCode, $statusMessage, $errors)
+    {
+        $this->errors = $errors;
+        $this->statusCode = $statusCode;
+        $this->statusMessage = $statusMessage;
+    }
+
+    /**
+     *
+     * Returns the http status code ie.: 400.
+     *
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     *
+     * Returns the http status code description: ie.: 'Bad Request'.
+     *
+     * @return string
+     */
+    public function getStatusMessage()
+    {
+        return $this->statusMessage;
+    }
+
+    /**
+     *
+     * Returns the list of errors returned by the API.
+     *
+     * @return Error[]
+     *
+     * @see \Moip\Exceptions\Error
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function __toString()
+    {
+        $template = "[$this->statusMessage] The following errors ocurred:\n%s";
+        $temp_list = "";
+        foreach($this->errors as $error){
+            $path = $error->getPath();
+            $desc = $error->getDescription();
+
+            $temp_list .= "$path: $desc\n";
+        }
+
+        return sprintf($template, $temp_list);
+    }
+
+
+}
