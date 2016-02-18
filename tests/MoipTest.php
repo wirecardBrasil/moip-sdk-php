80365d8f56a28942f8fc1a58d3facb8c7d50b41f
Gabriel Menezes <gabrielmenezesvi@gmail.com>
2016-02-18 13:33:37 -0200

    Remove redundant tests because if the constant value changed we would've to change the value in the test code.

--- tests/MoipTest.php
+++ tests/MoipTest.php
@@ -9,38 +9,6 @@ use Mockery as m;
  */
 class MoipTest extends MoipTestCase
 {
-    /**
-     * Test if endpoint production is valid.
-     */
-    public function testShouldReceiveEndpointProductionIsValid()
-    {
-        $endpoint_production = 'api.moip.com.br';
-        $const_endpoint_production = constant('\Moip\Moip::ENDPOINT_PRODUCTION');
-
-        $this->assertEquals($endpoint_production, $const_endpoint_production);
-    }
-
-    /**
-     * Test if endpoint sandbox is valid.
-     */
-    public function testShouldReceiveSandboxProductionIsValid()
-    {
-        $endpoint_sandbox = 'sandbox.moip.com.br';
-        $const_endpoint_sandbox = constant('\Moip\Moip::ENDPOINT_SANDBOX');
-
-        $this->assertEquals($endpoint_sandbox, $const_endpoint_sandbox);
-    }
-
-    /**
-     * Test if const CLIENT is valid.
-     */
-    public function testShouldReceiveClientIsValid()
-    {
-        $client = 'Moip SDK';
-        $const_client = constant('\Moip\Moip::CLIENT');
-
-        $this->assertEquals($client, $const_client);
-    }
 
     /**
      * test create connection.
