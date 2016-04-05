74ad1bfde604bfb87330ac1fa63372b0b6f86a8b
Turox <mushrambu@hotmail.com>
2016-04-05 19:37:07 -0300

    Update Customer.php
    PHP lança Undefined property: stdClass::$taxDocument na tentativa de criar um Customer sem o atributo taxDocument

--- src/Resource/Customer.php
+++ src/Resource/Customer.php
@@ -245,8 +245,8 @@ class Customer extends MoipResource
         $customer->data->phone->number = $this->getIfSet('number', $phone);
         $customer->data->birthDate = $this->getIfSet('birthDate', $response);
         $customer->data->taxDocument = new stdClass();
-        $customer->data->taxDocument->type = $this->getIfSet('type', $response->taxDocument);
-        $customer->data->taxDocument->number = $this->getIfSet('number', $response->taxDocument);
+        $customer->data->taxDocument->type = $this->getIfSet('type', @$response->taxDocument);
+        $customer->data->taxDocument->number = $this->getIfSet('number', @$response->taxDocument);
         $customer->data->addresses = [];
         $customer->data->shippingAddress = $this->getIfSet('shippingAddress', $response);
         $customer->data->billingAddress = $this->getIfSet('billingAddress', $response);
