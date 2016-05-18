d5ebee605d04aba5cb2d47ae298bc25cea57beaf
Jean Cesar Garcia <jeancesargarcia@gmail.com>
2016-05-18 01:17:00 -0300

    create function setDelayCapture

--- src/Resource/Payment.php
+++ src/Resource/Payment.php
@@ -126,6 +126,7 @@ class Payment extends MoipResource
 
         $payment->data->id = $this->getIfSet('id', $response);
         $payment->data->status = $this->getIfSet('status', $response);
+        $payment->data->delayCapture = $this->getIfSet('delayCapture', $response);
         $payment->data->amount = new stdClass();
         $payment->data->amount->total = $this->getIfSet('total', $response->amount);
         $payment->data->amount->currency = $this->getIfSet('currency', $response->amount);
@@ -334,13 +335,29 @@ class Payment extends MoipResource
     }
 
     /**
+     * Set delay capture.
+     *
+     * @return $this
+     */
+    public function setDelayCapture()
+    {
+        $this->data->delayCapture = true;
+
+        return $this;
+    }
+
+    /**
      * Set Multiorders.
      *
      * @param \Moip\Resource\Multiorders $multiorder
+     *
+     * @return $this
      */
     public function setMultiorder(Multiorders $multiorder)
     {
         $this->multiorder = $multiorder;
+
+        return $this;
     }
 
     /**
