a57c5a4c6137adddfdd250332eeb9685e50384d8
Phelipe Rocha <phelipeafonso@gmail.com>
2016-12-13 14:22:53 -0200

    Update Payment.php

--- src/Resource/Payment.php
+++ src/Resource/Payment.php
@@ -251,7 +251,7 @@ class Payment extends MoipResource
         $this->data->fundingInstrument->creditCard->holder->phone->countryCode = $holder->getPhoneCountryCode();
         $this->data->fundingInstrument->creditCard->holder->phone->areaCode = $holder->getPhoneAreaCode();
         $this->data->fundingInstrument->creditCard->holder->phone->number = $holder->getPhoneNumber();
-		$this->data->fundingInstrument->creditCard->holder->billingAddress = $holder->getBillingAddress();
+	$this->data->fundingInstrument->creditCard->holder->billingAddress = $holder->getBillingAddress();
     }
 
     /**
