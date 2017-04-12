ccdfa615d9d74e7c8103da214f8c79f3eea1e29e
Jean C. Garcia <jeancesargarcia@gmail.com>
2017-04-12 20:47:17 -0300

    create helpers

---
+++ src/Helper/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * convert a money amount (represented by a float or string (based on locale) ie.: R$ 5,00) to cents (represented by an int).
+ *
+ * @param float $amount
+ *
+ * @throws \UnexpectedValueException
+ *
+ * @return int
+ */
+function toCents(float $amount): int
+{
+    return Moip\Helper\Utils::toCents($amount);
+}
