40542c426053ac1c1cb195d0235c24d9004dd060
Jean C. Garcia <jeancesargarcia@gmail.com>
2017-04-12 20:51:39 -0300

    if function not exists

--- src/Helper/helpers.php
+++ src/Helper/helpers.php
@@ -1,15 +1,17 @@
 <?php
 
-/**
- * convert a money amount (represented by a float or string (based on locale) ie.: R$ 5,00) to cents (represented by an int).
- *
- * @param float $amount
- *
- * @throws \UnexpectedValueException
- *
- * @return int
- */
-function toCents(float $amount): int
-{
-    return Moip\Helper\Utils::toCents($amount);
+if (! function_exists('toCents')) {
+    /**
+     * convert a money amount (represented by a float or string (based on locale) ie.: R$ 5,00) to cents (represented by an int).
+     *
+     * @param float $amount
+     *
+     * @throws \UnexpectedValueException
+     *
+     * @return int
+     */
+    function toCents(float $amount): int
+    {
+        return Moip\Helper\Utils::toCents($amount);
+    }
 }
