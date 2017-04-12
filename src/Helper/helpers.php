c3ed0e868f5f63112bc3594254d4a6ac200820f2
Jean C. Garcia <jeancesargarcia@gmail.com>
2017-04-12 20:54:35 -0300

    create function pr

--- src/Helper/helpers.php
+++ src/Helper/helpers.php
@@ -15,3 +15,26 @@ if (! function_exists('to_cents')) {
         return Moip\Helper\Utils::toCents($amount);
     }
 }
+
+if (! function_exists('pr')) {
+    /**
+     * print_r() convenience function.
+     *
+     * In terminals this will act similar to using print_r() directly, when not run on cli
+     * print_r() will also wrap <pre> tags around the output of given variable. Similar to debug().
+     *
+     * This function returns the same variable that was passed.
+     *
+     * @param mixed $var Variable to print out.
+     * @return mixed the same $var that was passed to this function
+     * @link http://book.cakephp.org/3.0/en/core-libraries/global-constants-and-functions.html#pr
+     * @see debug()
+     */
+    function pr($var)
+    {
+        $template = '<pre class="pr">%s</pre>';
+        printf($template, trim(print_r($var, true)));
+
+        return $var;
+    }
+}
