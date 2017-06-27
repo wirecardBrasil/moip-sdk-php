d336f9f04dc92a978e3d67942091b573c9a30643
Caio Gaspar <caio1309@hotmail.com>
2017-06-27 11:54:26 -0300

    Fix bank account refund

---
+++ tests/Resource/RefundTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Moip\Tests\Resource;
+
+use Moip\Tests\TestCase;
+
+class RefundTest extends TestCase
+{
+
+    public function testRefundBankAccountFull() {
+        $this->mockHttpSession($this->body_order);
+        $order = $this->createOrder()->create();
+        $this->mockHttpSession($this->body_billet_pay);
+        $payment = $order->payments()->get('PAY-N2TXFLV4PP7Y'); 
+        $this->mockHttpSession($this->body_refund_full_bankaccount);
+        $refund = $payment->refunds()
+            ->bankAccountFull('CHECKING', '001', '1584', '9', '00210169', '6', $order->getCustomer());
+        $this->assertNotEmpty($refund->getId());
+    }
+    
+    public function testRefundBankAccountPartial() {
+        $this->mockHttpSession($this->body_order);
+        $order = $this->createOrder()->create();
+        $this->mockHttpSession($this->body_billet_pay);
+        $payment = $order->payments()->get('PAY-N2TXFLV4PP7Y'); 
+        $this->mockHttpSession($this->body_refund_partial_bankaccount);
+        $refund = $payment->refunds()
+            ->bankAccountPartial(20000, 'SAVING','001', '1584', '9', '00210169', '6', $order->getCustomer());
+        $this->assertNotEmpty($refund->getId());
+    }
+}
