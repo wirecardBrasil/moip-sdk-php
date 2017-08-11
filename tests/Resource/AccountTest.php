db9a4999299235ed23987776689f316009a3dc66
Caio Gaspar <caio.gaspar@moip.com.br>
2017-08-11 10:45:30 -0300

    fix(Account): Fix account create without company set
    Preventing account from create with company node when it was not set

--- tests/Resource/AccountTest.php
+++ tests/Resource/AccountTest.php
@@ -8,8 +8,30 @@ use Moip\Tests\TestCase;
  * Description of AccountTest.
  */
 class AccountTest extends TestCase
-{
+{    
     public function testShouldCreateAccount()
+    {
+        $this->mockHttpSession($this->body_moip_account);
+
+        $account = $this->moip->accounts()
+            ->setName('Fulano')
+            ->setLastName('De Tal')
+            ->setEmail('fulano@email2.com')
+            ->setIdentityDocument('4737283560', 'SSP', '2015-06-23')
+            ->setBirthDate('1988-12-30')
+            ->setTaxDocument('16262131000')
+            ->setType('MERCHANT')
+            ->setPhone(11, 66778899, 55)
+            ->addAlternativePhone(11, 66448899, 55)
+            ->addAlternativePhone(11, 66338899, 55)
+            ->setTransparentAccount(true)
+            ->addAddress('Rua de teste', 123, 'Bairro', 'Sao Paulo', 'SP', '01234567', 'Apt. 23', 'BRA')
+            ->create();
+
+        $this->assertNotEmpty($account->getId());
+    }
+    
+    public function testShouldCreateAccountWithCompany()
     {
         $this->mockHttpSession($this->body_moip_account);
 
