0ed30df011bc552230c0cc67a965a0e345ef46e8
Caio Gaspar <caio1309@hotmail.com>
2017-08-11 11:18:31 -0300

    fix(Account): Fix account create without company set (#157)
    * fix(Account): Fix account create without company set
    Preventing account from create with company node when it was not set
    
    * style(Account): Fix formatting to StyleCI
    
    Fix formatting to StyleCI
    
    * style(Account): Fix formatting to StyleCI
    
    Fix formatting to StyleCI

--- tests/Resource/AccountTest.php
+++ tests/Resource/AccountTest.php
@@ -26,6 +26,28 @@ class AccountTest extends TestCase
             ->addAlternativePhone(11, 66338899, 55)
             ->setTransparentAccount(true)
             ->addAddress('Rua de teste', 123, 'Bairro', 'Sao Paulo', 'SP', '01234567', 'Apt. 23', 'BRA')
+            ->create();
+
+        $this->assertNotEmpty($account->getId());
+    }
+
+    public function testShouldCreateAccountWithCompany()
+    {
+        $this->mockHttpSession($this->body_moip_account);
+
+        $account = $this->moip->accounts()
+            ->setName('Fulano')
+            ->setLastName('De Tal')
+            ->setEmail('fulano@email2.com')
+            ->setIdentityDocument('4737283560', 'SSP', '2015-06-23')
+            ->setBirthDate('1988-12-30')
+            ->setTaxDocument('16262131000')
+            ->setType('MERCHANT')
+            ->setPhone(11, 66778899, 55)
+            ->addAlternativePhone(11, 66448899, 55)
+            ->addAlternativePhone(11, 66338899, 55)
+            ->setTransparentAccount(true)
+            ->addAddress('Rua de teste', 123, 'Bairro', 'Sao Paulo', 'SP', '01234567', 'Apt. 23', 'BRA')
             ->setCompanyName('Empresa Teste', 'Teste Empresa ME')
             ->setCompanyOpeningDate('2011-01-01')
             ->setCompanyPhone(11, 66558899, 55)
